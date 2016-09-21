3cc4824801b528589d24701a7fe2abfcb08195fb
alkohl <1520339807@qq.com>
2016-09-21 22:15:12 +0800

    创建Aniaml类和RecordInf类

---
+++ src/animal/Animal.java
@@ -0,0 +1,30 @@
+package animal;
+
+/**
+ * Created by sunnior on 9/21/16.
+ */
+public class Animal {
+    private String id;
+    private int x;
+    private int y;
+
+    public String getId() {
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
